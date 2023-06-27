b68ec948744a90a54b8674487bc3c8a6f52cb35b
Keonwoo Kim <keonwookim@Keonwooui-MacBookPro.local>
2023-06-28 01:50:52 +1200

    End of Section 13

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,13 +1,11 @@
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet] //api/activities
--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { request } from "http";
 import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
+import { User, UserFormValues } from "../models/user";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 
@@ -13,6 +14,14 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+  const token = store.commonStore.token;
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+})
+
 axios.interceptors.response.use(
   async (response) => {
     await sleep(1000);
@@ -22,8 +31,8 @@ axios.interceptors.response.use(
     const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
       case 400:
-        if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
-            router.navigate('/not-found');
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          router.navigate("/not-found");
         }
         if (data.errors) {
           const modalStateErrors = [];
@@ -55,8 +64,6 @@ axios.interceptors.response.use(
   }
 );
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
-
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
   post: <T>(url: string, body: {}) =>
@@ -74,8 +81,16 @@ const Activities = {
   delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 };
 
+const Account = {
+  current: () => requests.get<User>("/account"),
+  login: (user: UserFormValues) => requests.post<User>("/account/login", user),
+  register: (user: UserFormValues) =>
+    requests.post<User>("account/register", user),
+};
+
 const agent = {
   Activities,
+  Account,
 };
 
 export default agent;
