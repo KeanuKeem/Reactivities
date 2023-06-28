3aa3d4b7e532531924c63740767a5366496205ab
Keonwoo Kim <keonwookim@Keonwooui-MacBookPro.local>
2023-06-28 13:42:03 +1200

    End of Section 14

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,9 @@ namespace API.DTOs
         [Required]
         public string Email { get; set; }
         [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string DisplayName { get; set; }
         [Required]
         public string Username { get; set; }
